a0b42d464625e4a24d36dd9b23e82c9112ac90db
Luke-shih <s1415937@gmail.com>
2021-09-04 10:32:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize'
 const SCOPE = 'user,repo'
 
-const client_id = '127c617fd4d8ce08fdd8'
+const client_id = ''
 
 module.exports = {
     github: {
         request_token_url: 'https://github.com/login/oauth/access_token',
         client_id,
-        client_secret: 'c5da3e77a2ef3c8f719c7acf50f884eab1d7e7d5',
+        client_secret: '',
     },
     GITHUB_OAUTH_URL,
     OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${client_id}&scope=${SCOPE}`
